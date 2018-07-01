c16f669fe877a33607ab1917b91c95fb716da3f3
Merdet <super.den2005@ukr.net>
2018-07-01 16:47:50 +0300

    [python]

---
+++ python.py
@@ -0,0 +1,244 @@
+Python 3.7.0 (v3.7.0:1bf9cc5093, Jun 27 2018, 04:06:47) [MSC v.1914 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> number = int(input('Enter a number'))
+if number > 1
+    print ("1")
+elif number <1
+    print("-1")
+else number = 1
+    print ("1")
+    
+SyntaxError: multiple statements found while compiling a single statement
+>>> number = int(input('Enter a number'))
+Enter a number
+Traceback (most recent call last):
+  File "<pyshell#1>", line 1, in <module>
+    number = int(input('Enter a number'))
+ValueError: invalid literal for int() with base 10: ''
+>>> number = int(input('Enter a number'))
+if number > 1
+    print ("1")
+elif number <1
+    print("-1")
+else number = 1
+    print ("1")
+KeyboardInterrupt
+>>> number = int(input('Enter a number'))
+
+Enter a number5
+>>> if number > 1
+    print ("1")
+elif number <1
+    print("-1")
+else number = 1
+    print ("1")
+    
+SyntaxError: invalid syntax
+>>> if number > 1
+SyntaxError: invalid syntax
+>>> if number >1
+SyntaxError: invalid syntax
+>>> if number > 1
+    print ("1")
+    
+SyntaxError: invalid syntax
+>>> if number > 1
+    print ("1")
+    
+SyntaxError: invalid syntax
+>>> if number => 1
+SyntaxError: invalid syntax
+>>> if number > 1:
+    print ("1")
+elif number <1:
+    print("-1")
+else number = 1:
+    print ("1")
+    
+SyntaxError: invalid syntax
+>>> if number > 1
+    print ("1")
+elif number <1
+    print("-1")
+elif number = 1
+    print ("1")
+    
+SyntaxError: invalid syntax
+>>> if number > 1:
+    print ("1")
+elif number <1:
+    print("-1")
+elif number = 1:
+    print ("1")
+    
+SyntaxError: invalid syntax
+>>>  if number > 1:
+    print ("1")
+elif number <1:
+    print("-1")
+elif number ==1:
+    print ("1")
+    
+SyntaxError: unexpected indent
+>>> if number > 1:
+    print ("1")
+elif number <1:
+    print("-1")
+elif number = 1:
+    print ("1")
+    
+SyntaxError: invalid syntax
+>>> if number > 1:
+    print ("1")
+elif number <1:
+    print("-1")
+elif number ==1:
+    print ("1")
+else:
+	not correct
+1
+SyntaxError: invalid syntax
+>>> if number > 1:
+    print ("1")
+elif number <1:
+    print("-1")
+elif number ==1:
+    print ("1")
+else:
+	not correct
+
+1
+>>> 1
+1
+>>> 3
+3
+>>> number = 3
+>>> number
+3
+>>> if number > 1:
+    print ("1")
+elif number <1:
+    print("-1")
+elif number ==1:
+    print ("1")
+else:
+	print ("not correct")
+
+1
+>>> 1
+1
+>>> 3
+3
+>>> number
+3
+>>> if number > 1:
+    print ("1")
+elif number <1:
+    print("-1")
+elif number ==1:
+    print ("1")
+else:
+	not correctif number > 1:
+	    print ("1")
+	elif number <1:
+	    print("-1")
+	elif number ==1:
+	    print ("1")
+	else:
+		print ("not correct")
+		
+SyntaxError: invalid syntax
+>>> if number > 1:
+    print ("1")
+elif number <1:
+    print("-1")
+elif number ==1:
+    print ("1")
+else:
+	print ("not correct")
+
+	
+1
+>>> number = "-2"
+>>> if number > 1:
+    print ("1")
+elif number <1:
+    print("-1")
+elif number ==1:
+    print ("1")
+else:
+	print ("not correct")
+
+	
+Traceback (most recent call last):
+  File "<pyshell#34>", line 1, in <module>
+    if number > 1:
+TypeError: '>' not supported between instances of 'str' and 'int'
+>>> if number > 1:
+    print ("1")
+elif number <1:
+    print("-1")
+elif number ==1:
+    print ("1")
+else:
+	print ("not correct")
+
+Traceback (most recent call last):
+  File "<pyshell#36>", line 1, in <module>
+    if number > 1:
+TypeError: '>' not supported between instances of 'str' and 'int'
+>>> if x > 1:
+    print ("1")
+elif x <1:
+    print("-1")
+elif x ==1:
+    print ("1")
+else:
+	print ("not correct")
+
+	
+Traceback (most recent call last):
+  File "<pyshell#38>", line 1, in <module>
+    if x > 1:
+NameError: name 'x' is not defined
+>>> x = 2
+>>> if x > 1:
+    print ("1")
+elif x <1:
+    print("-1")
+elif x ==1:
+    print ("1")
+else:
+	print ("not correct")
+
+	
+1
+>>> x = -34
+>>> if number > 1:
+    print ("1")
+elif number <1:
+    print("-1")
+elif number ==1:
+    print ("1")
+else:
+	print ("not correct")if x > 1:
+    print ("1")
+elif x <1:
+    print("-1")
+elif x ==1:
+    print ("1")
+else:
+	print ("not correct")'
+	
+SyntaxError: invalid syntax
+>>> if x > 1:
+    print ("1")
+elif x <1:
+    print("-1")
+elif x ==1:
+    print ("1")
+else:
+	print ("not correct")
+
+-1
+>>> 
